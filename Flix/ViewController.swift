7179d66f799b7a1f4cdca3fe2607e4d4374662c0
Prerana Sunilkumar <preranasunilkumar@preranas-macbook-pro.local>
2019-02-03 16:32:21 -0800

    Initial Commit

---
+++ Flix/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Flix
+//
+//  Created by Prerana Sunilkumar on 2/3/19.
+//  Copyright © 2019 Prerana Sunilkumar. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
