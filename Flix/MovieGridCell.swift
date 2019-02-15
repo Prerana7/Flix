9a3ad56d2986ec9e5ee7436f9bf63acc0489667a
Prerana Sunilkumar <preranasunilkumar@preranas-macbook-pro.local>
2019-02-14 22:26:29 -0800

    Flix part 2

---
+++ Flix/MovieGridCell.swift
@@ -0,0 +1,14 @@
+//
+//  MovieGridCell.swift
+//  Flix
+//
+//  Created by Prerana Sunilkumar on 2/14/19.
+//  Copyright © 2019 Prerana Sunilkumar. All rights reserved.
+//
+
+import UIKit
+class MovieGridCell: UICollectionViewCell {
+ 
+    @IBOutlet weak var posterView: UIImageView!
+    
+}
--- Flix/MoviesViewController.swift
+++ Flix/MoviesViewController.swift
@@ -73,14 +73,14 @@ class MoviesViewController: UIViewController, UITableViewDataSource, UITableView
     }
     
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+        let cell = sender as! UITableViewCell
+        let indexPath = tableView.indexPath(for: cell)!
+        let movie = movies[indexPath.row]
+        let detailViewControler = segue.destination as! DetailViewController
+        detailViewControler.movie = movie
+        
+        tableView.deselectRow(at: indexPath, animated: true)
     }
-    */
 
 }
